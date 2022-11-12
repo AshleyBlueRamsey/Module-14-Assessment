9c7687779acb7cc99121376460f65406219e3238
Ashley Ramsey <114588956+AshleyBlueRamsey@users.noreply.github.com>
2022-11-12 18:27:45 -0500

    Update solution.test.js
    Updated code to include all required tests

--- test/solution.test.js
+++ test/solution.test.js
@@ -11,22 +11,22 @@ describe("partitionStudentsByScore", () => {
      it("should return students partitioned into two arrays based on score", () => {
         const expected = [
             [
-                { name: "Morgan Sutton", score: 7.4},
+                { name: "Morgan Sutton", score: 7.4 },
             ],
             [
-                { name: "Leo Yeon-Joo", score: 8.9},
-                { name: "Natalee Vargas", score: 9.2},
+                { name: "Leo Yeon-Joo", score: 8.9 },
+                { name: "Natalee Vargas", score: 9.2 },
             ],
         ];
-        const actual = partitionStudentsByScore(students, 8.4);
+        const actual = partitionStudentsByScore(students, 8);
         expect(actual).to.eql(expected);
     });
 
     it("should return an array of two empty arrays if the student list is empty", () => {
-        const students = [];
-        const expected = [[], []];
-        const actual = partitionStudentsByScore(students, 6);
-        expect(actual).to.eql(expected);
+      const students = [];
+      const expected = [[], []];
+      const actual = partitionStudentsByScore(students, 0);
+      expect(actual).to.eql(expected);
     });
     it("should put all students into first array if the scores are appropriate", () => {
         const expected = [
@@ -40,5 +40,19 @@ describe("partitionStudentsByScore", () => {
         const actual = partitionStudentsByScore(students, 9.5);
         expect(actual).to.eql(expected);
     });
+  
+  it("should put all students into second array if the scores are appropriate", () => {
+    const expected = [
+      [],
+      [
+        { name: "Leo Yeon-Joo", score: 8.9 },
+        { name: "Morgan Sutton", score: 7.4 },
+        { name: "Natalee Vargas", score: 9.2},
+      ],
+    ];
+    const actual = partitionStudentsByScore(students, 4);
+    expect(actual).to.eql(expected);
+  });
 
 });
+
